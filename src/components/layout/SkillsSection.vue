9c79ef180b6091c968ead66fc9b0d01f2d9af33a
Maksim Pikovskiy <pikmak2001@gmail.com>
2025-11-04 20:50:28 +0000

    Implemented category filtering in SkillsSection and updated skills data

--- src/components/layout/SkillsSection.vue
+++ src/components/layout/SkillsSection.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import { skills } from '@/data/skills'
 import SectionHeading from '@/components/ui/SectionHeading.vue'
+
+const skillCategories = skills.reduce((categories, skill) => {
+  if (!categories.includes(skill.category)) {
+    categories.push(skill.category)
+  }
+  return categories
+}, [] as string[])
+
+const selectedCategory = ref('all')
+
+const changeCategory = (category: string) => {
+  if (selectedCategory.value === category) {
+    selectedCategory.value = 'all'
+    return
+  }
+  selectedCategory.value = category
+}
+
+const filteredSkills = computed(() => {
+  if (selectedCategory.value === 'all') {
+    return skills
+  }
+  return skills.filter((skill) => skill.category === selectedCategory.value)
+})
 </script>
 
 <template>
@@ -8,11 +33,26 @@ import SectionHeading from '@/components/ui/SectionHeading.vue'
     <div class="container mx-auto px-4 md:px-6">
       <SectionHeading title="My Skills" subtitle="Technologies and tools I have worked with" />
 
+      <div class="flex flex-row gap-2 mb-6 justify-center">
+        <button
+          v-for="category in skillCategories"
+          :key="category"
+          @click="changeCategory(category)"
+          class="px-3 py-1 rounded-full text-md font-medium bg-neutral-100 dark:bg-neutral-700 hover:bg-primary-600 dark:hover:bg-primary-400 text-neutral-700 dark:text-neutral-300 hover:text-white hover:dark:text-neutral-700 capitalize"
+          :class="
+            selectedCategory === category &&
+            'bg-primary-600 dark:bg-primary-400 hover:bg-primary-700 dark:hover:bg-primary-500 text-white dark:text-neutral-700 font-medium'
+          "
+        >
+          {{ category.replace('-', ' ') }}
+        </button>
+      </div>
+
       <div
         class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 appear-animation"
       >
         <div
-          v-for="skill in skills"
+          v-for="skill in filteredSkills"
           :key="skill.name"
           class="flex flex-col items-center p-6 bg-white dark:bg-neutral-800 rounded-xl shadow-sm hover:shadow-md transition-shadow"
         >
